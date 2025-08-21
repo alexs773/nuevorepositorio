7e8430b7cd1b4c4ce4cba467c0affae93eb1b011
alexs773 <alexdk828732@gmail.com>
2025-08-21 10:44:23 -0500

    zxcxc

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
@@ -0,0 +1,9 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplication {
+    public static void main(String[] args) {
+        System.out.println("INICIANDO ASISTENCIA PRUEBA...");
+        AsistenciaApplication.main(args);
+    }
+}
+
