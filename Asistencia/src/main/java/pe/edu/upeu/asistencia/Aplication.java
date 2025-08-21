a0dfa85a6a2103fcf6d9c9621c955cfb6e9bfe45
alexs773 <alexdk828732@gmail.com>
2025-08-21 11:29:06 -0500

    segundo cambio

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
@@ -2,7 +2,10 @@ package pe.edu.upeu.asistencia;
 
 public class Aplication {
     public static void main(String[] args) {
-        System.out.println("INICIANDO ASISTENCIA PRUEBA...");
+        System.out.println("INICIANDO ASISTENCIA PRUEBA..." +
+                "......................................." +
+                "......................................." +
+                ".......................................");
         AsistenciaApplication.main(args);
     }
 }
